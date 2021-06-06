561360e2e261643aa331b0fab376572c559d2928
Selvam B <84901905+selvam19@users.noreply.github.com>
2021-06-06 12:13:38 +0530

    Add files via upload

---
+++ Addtwonumber.java
@@ -0,0 +1,10 @@
+//sum of two number
+public class Addtwonumber
+{
+    public static void main(String[] args)
+    {
+        int firstnumber=10,secondnumer=20;
+        int c= firstnumber+secondnumer;
+        System.out.println(c);
+    }
+}
