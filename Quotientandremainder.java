3831913f8a79659f7bf2607bf4ba518877368084
Selvam B <84901905+selvam19@users.noreply.github.com>
2021-06-06 12:20:33 +0530

    Add files via upload

---
+++ Quotientandremainder.java
@@ -0,0 +1,12 @@
+//quotient and remainder
+public class Quotientandremainder
+{
+    public static void main(String[] args)
+    {
+        int firstnumber=100,secondnumber=2;
+        int quotient=firstnumber / secondnumber;
+        int remainder=firstnumber % secondnumber;
+        System.out.println("quotient number is: "+quotient);
+        System.out.println("remainder number is: "+remainder);
+    }
+}
