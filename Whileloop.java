8b9db70f3409cf93cd74c678ff90c4194a47a5b3
Selvam B <84901905+selvam19@users.noreply.github.com>
2021-06-07 07:24:33 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Whileloop 
+{
+	public static void main(String[] args)
+	{
+		char operator;
+		double num1, num2, result;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choose an Operator: +, -, *, /");
+		operator = sc.next().charAt(0);
+		
+		System.out.println("Enter First Number: ");
+		num1 = sc.nextDouble();
+		System.out.println("Enter Second Number: ");
+		num2 = sc.nextDouble();
+		switch (operator) 
+		{
+		case '+':
+			result = num1 + num2;
+			System.out.println(num1 + " + " + num2 + " = " + result);
+			break;
+		case '-':
+			result = num1 - num2;
+			System.out.println(num1+ " - " + num2 + " = " + result);
+			break;
+		case '*':
+			result = num1 * num2;
+			System.out.println(num1 + " * " + num2 + " = " + result);
+			break;
+		case '/':
+			result = num1 / num2;
+			System.out.println(num1 + " / " + num2 + " = " + result);
+			break;
+		default:
+			System.out.println("Invalid Operator!!!");
+			break;
+		}
+	  sc.close();
+	}
+}
